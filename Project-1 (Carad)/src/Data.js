7e320d5f70b5f686bcb99c0085af1095952b68c2
virat1816 <viratpatel5880@gmail.com>
2023-10-31 09:35:44 +0530

    Card

---
+++ Project-1 (Carad)/src/Data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald"
+    },
+    {
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee"
+    },
+    {
+        "title": "Harry Potter and the Sorcerer's Stone",
+        "author": "J.K. Rowling"
+    },
+    {
+        "title": "The Catcher in the Rye",
+        "author": "J.D. Salinger"
+    }
+]
