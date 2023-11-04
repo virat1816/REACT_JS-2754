ab8bc565fd086e9a392b7077a1618556797418a3
virat1816 <viratpatel5880@gmail.com>
2023-11-04 12:51:47 +0530

    Website in React

---
+++ React js_Seppo/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import Header from "./Header";
+import Home from "./Home";
+// import Footer from "./Footer";
+
+const App = () => {
+  return (
+    <>
+    {/* <Header/> */}
+    <Home/>
+    {/* <Footer/> */}
+    </>
+    );
+}
+
+export default App;
